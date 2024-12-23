c58bf122fb5367e35ccab40f9bac7b0632720f53
henok dagne <henokdagne19@gmail.com>
2024-12-23 22:13:31 +0300

    Add even_number

--- task.py
+++ task.py
@@ -3,6 +3,11 @@ def sum_number(number):
     for i in range(len(number)):
         sum += number[i]
     return sum
+def even_number():
+    for i in range(0,20, 2):
+        if i != 0:
+            print(i, end=' ')
 
 num = [1, 2, 3, 4, 5, 6]
 print("sum:", sum_number(num))
+even_number()
